4d18ce2700ea7d96d778f66ab5968cfacf6a0fba
sohilpandya <sohilpandya@me.com>
2015-10-07 20:45:33 +0100

    trying to implement callbacks and failing

--- database.js
+++ database.js
@@ -3,25 +3,31 @@ var client = redis.createClient();
 var db = {};
 
 
-  db.addPostRedis = function(date, username, post) {
+  db.addPostRedis = function(date, username, post, res) {
     client.hmset(date, username, post);
   };
 
 
-  db.addDateToList = function(date) {
+  db.addDateToList = function(date, username, post, res) {
     client.rpush(["Dates", date]);
   };
 
 
 
-  db.tenFromList = function(callback) {
+  db.tenFromList = function(date, username, post, res) {
     client.lrange('Dates', 0, -1, function(err, reply) {
-
+      console.log(reply);
 			var pusharr = [];
 			for(var i = reply.length-1;i>reply.length-11;i--){
 				// console.log(db.getPostByDate(reply[i]));
-				pusharr.push(db.getPostByDate(reply[i]));
+        var last = false;
+        if (i===reply.length-11) {
+          last=true;
+        }
+        pusharr.push(db.getPostByDate(last,reply[i],username,post,res));
+
 			}
+      // console.log(pusharr);
 
 			//return reply;
 			// console.log(pusharr);
@@ -30,10 +36,15 @@ var db = {};
 
 };
 
-db.getPostByDate = function(date) {
+db.getPostByDate = function(last, date, username, post, res) {
   client.hgetall(date, function(err, object) {
     console.log(JSON.stringify(object) + " TESTING");
-    return JSON.stringify(object);
+    if (last===false){
+      res.write(JSON.stringify(object));
+    } else{
+      res.end();
+    }
+
 
   });
 };
