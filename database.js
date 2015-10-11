919ebe07169664c888a7da3a39ad0935926a3d1a
tormod17 <tormodsmith@gmail.com>
2015-10-11 22:03:33 +0100

    adding frontend tests

--- database.js
+++ database.js
@@ -1,45 +1,47 @@
 var redis = require('redis');
-var client = redis.createClient(process.env.REDIS_URL, {no_ready_check: true});
+var client = redis.createClient(process.env.REDIS_URL, {
+    no_ready_check: true
+});
 var db = {};
 
 
 
-  db.addPostRedis = function(date, username, post, storeNo, res) {
-    client.hmset(date, "username", username, "post", post,"storeNo",storeNo,"date",date);
-  };
+db.addPostRedis = function(date, username, post, storeNo, res) {
+    client.hmset(date, "username", username, "post", post, "storeNo", storeNo, "date", date);
+};
 
 
 db.addDateToList = function(date, username, post, res) {
-  client.rpush(["Dates", date]);
+    client.rpush(["Dates", date]);
 
 };
 
-db.delPost = function(date){
+db.delPost = function(date, username, post, res) {
 
-    client.del(date, function (err,reply) {
-      // body...
+    client.del(date, function(err, reply) {
+       console.log(reply + " in delete function");
     });
 };
 
 
 db.tenFromList = function(date, username, post, res) {
-  client.lrange('Dates', 0, -1, function(err, reply) {
-    var pusharr = [];
-    count = 0;
-    for (var i = reply.length - 1; i > reply.length - 11; i--) {
-      var frontarr = [];
-      //console.log(db.getPostByDate(reply[i]));
-      client.hgetall(reply[i], function(err, object) {
-        frontarr.push(object);
-        count++;
-        if (count === 10) {
-          // console.log(frontarr);
-          res.end(JSON.stringify(frontarr));
-        }
-      });
+    client.lrange('Dates', 0, -1, function(err, reply) {
+        var pusharr = [];
+        count = 0;
+        for (var i = reply.length - 1; i > reply.length - 11; i--) {
+            var frontarr = [];
+            //console.log(db.getPostByDate(reply[i]));
+            client.hgetall(reply[i], function(err, object) {
+                frontarr.push(object);
+                count++;
+                if (count === 10) {
+                    // console.log(frontarr);
+                    res.end(JSON.stringify(frontarr));
+                }
+            });
 
-    }
-  });
+        }
+    });
 };
 
 module.exports = db;
